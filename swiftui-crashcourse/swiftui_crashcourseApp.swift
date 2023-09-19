eb45df0af0076067f938dffca066099e46c17ee8
Ahmed Gagan <67264609+adgan11@users.noreply.github.com>
2023-09-19 18:18:29 +0530

    Initial Commit

---
+++ swiftui-crashcourse/swiftui_crashcourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_crashcourseApp.swift
+//  swiftui-crashcourse
+//
+//  Created by Ahmed Gagan on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_crashcourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
