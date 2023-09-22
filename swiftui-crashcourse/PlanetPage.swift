ca7abba9564f2663e0743783733b88b0659d3fd3
Ahmed Gagan <67264609+adgan11@users.noreply.github.com>
2023-09-22 16:27:11 +0530

    first commit

---
+++ swiftui-crashcourse/PlanetPage.swift
@@ -0,0 +1,20 @@
+//
+//  PlanetPage.swift
+//  swiftui-crashcourse
+//
+//  Created by Ahmed Gagan on 21/09/23.
+//
+
+import SwiftUI
+
+struct PlanetPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlanetPage_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanetPage()
+    }
+}
---
+++ swiftui-crashcourse/PlanetsData.swift
@@ -0,0 +1,8 @@
+//
+//  PlanetsData.swift
+//  swiftui-crashcourse
+//
+//  Created by Ahmed Gagan on 21/09/23.
+//
+
+import Foundation
